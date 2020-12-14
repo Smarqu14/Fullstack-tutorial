222117d3034bc2682484af446811d3d52dc6def4
Steve Marquez <steveantonio00@gmail.com>
2020-12-13 18:40:46 -0800

    fetch data from the serverside

--- config.js
+++ config.js
@@ -4,6 +4,10 @@ export const nodeEnv = env.NODE_ENV || 'development';
 
 export default {
   port: env.PORT || 3001,
+  host: env.HOST || '0.0.0.0',
+  get serverUrl() {
+    return `http://${this.host}:${this.port}`;
+  },
 };
 
 export const logStars = function (message) {
--- server.js
+++ server.js
@@ -15,6 +15,7 @@ server.use(
 );
 server.set('view engine', 'ejs');
 
+import './serverRender';
 server.get('/', (req, res) => {
   res.render('index', {
     content: 'Hello world from <em>EJS</em>',
